93ba9276a51f3dbe41ddccf851c8faa57dcf658c
cofevtn6mt <v48denwa8b@gmail.com>
2022-04-22 05:54:09 +0900

    Fix ArgumentError on activerecord 7.0

--- lib/blouson/sensitive_table_query_log_silencer.rb
+++ lib/blouson/sensitive_table_query_log_silencer.rb
@@ -1,13 +1,13 @@
 module Blouson
   class SensitiveTableQueryLogSilencer < Arproxy::Base
-    def execute(sql, name=nil)
+    def execute(sql, name=nil, **kwargs)
       if Rails.logger.level != Logger::DEBUG || !(Blouson::SENSITIVE_TABLE_REGEXP === sql)
-        return super(sql, name)
+        return super(sql, name, **kwargs)
       end
 
       ActiveRecord::Base.logger.silence(Logger::INFO) do
         Rails.logger.info "  [Blouson::SensitiveTableQueryLogSilencer] SQL Log is skipped for sensitive table"
-        super(sql, name)
+        super(sql, name, **kwargs)
       end
     end
   end
