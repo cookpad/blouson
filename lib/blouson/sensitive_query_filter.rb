e465570d62c6aede6d83bdc7892cc6cacd768ecb
shogo-ohta <shogo-ohta@cookpad.com>
2024-08-11 14:05:45 +0900

    Remove original_exception param

--- lib/blouson/sensitive_query_filter.rb
+++ lib/blouson/sensitive_query_filter.rb
@@ -15,38 +15,27 @@ module Blouson
     end
 
     module StatementInvalidErrorFilter
-      def initialize(message = nil, original_exception = nil, sql: nil, binds: nil, connection_pool: nil)
-        if SensitiveQueryFilter.contain_sensitive_query?(message) || (SensitiveQueryFilter.contain_sensitive_query?(sql))
+      def initialize(message = nil, sql: nil, binds: nil, connection_pool: nil)
+        if SensitiveQueryFilter.contain_sensitive_query?(message) || SensitiveQueryFilter.contain_sensitive_query?(sql)
           message = SensitiveQueryFilter.filter_sensitive_words(message) if message
           sql = SensitiveQueryFilter.filter_sensitive_words(sql) if sql
           if defined?(Mysql2::Error)
-            if original_exception.is_a?(Mysql2::Error)
-              original_exception.extend(Mysql2Filter)
-            elsif $!.is_a?(Mysql2::Error)
+            if $!.is_a?(Mysql2::Error)
               $!.extend(Mysql2Filter)
             end
           end
         end
 
-        if original_exception
-          # Rails < 5.0
-          super(message, original_exception)
-        elsif connection_pool
+        if connection_pool
           # Rails >= 7.1
           #
           # - https://github.com/rails/rails/pull/48295
           super(message, sql: sql, binds: binds, connection_pool: connection_pool)
-        elsif sql
+        else
           # Rails >= 6.0
           #
           # - https://github.com/rails/rails/pull/34468
           super(message, sql: sql, binds: binds)
-        else
-          # Rails >= 5.0
-          #
-          # - https://github.com/rails/rails/pull/18774
-          # - https://github.com/rails/rails/pull/27503
-          super(message)
         end
       end
 
@@ -87,5 +76,5 @@ module Blouson
 
       private :log
     end
-    end
+  end
 end
