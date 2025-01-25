28f57010cf88cd48ad0890362676d4933d060528
shogo-ohta <shogo-ohta@cookpad.com>
2025-01-25 11:11:38 +0900

    Fix for arproxy v1

--- lib/blouson/sensitive_table_query_log_silencer.rb
+++ lib/blouson/sensitive_table_query_log_silencer.rb
@@ -1,13 +1,13 @@
 module Blouson
-  class SensitiveTableQueryLogSilencer < Arproxy::Base
-    def execute(sql, name=nil, **kwargs)
+  class SensitiveTableQueryLogSilencer < Arproxy::Proxy
+    def execute(sql, context)
       if !Rails.logger.debug? || !(Blouson::SENSITIVE_TABLE_REGEXP === sql)
-        return super(sql, name, **kwargs)
+        return super(sql, context)
       end
 
       ActiveRecord::Base.logger.silence(Logger::INFO) do
         Rails.logger.info "  [Blouson::SensitiveTableQueryLogSilencer] SQL Log is skipped for sensitive table"
-        super(sql, name, **kwargs)
+        super(sql, context)
       end
     end
   end
